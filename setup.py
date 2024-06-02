b221b645efa6f916ed3c8721f3bac61c0fc0f2f6
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-02 17:50:50 +0600

    test

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name='fetchhtml',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+    ],
+    author='Md Anisur Rahman',
+    author_email='anisurrahman06046@gmail.com',
+    description='A simple package to fetch HTML data from a website',
+    url='https://github.com/yourusername/fetchhtml',  # Update with your actual GitHub URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
